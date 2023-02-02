cc291c95546de7ce623db64831da488875d69d85
FastV1per <SLeano863@gmail.com>
2023-02-02 15:51:12 -0800

    update

--- programs/21codons.py
+++ programs/21codons.py
@@ -8,8 +8,8 @@ dna = 'ATAGCGAATATCTCTCATGAGAGGGAA'
 
 '''
 dna = 'ATAGCGAATATCTCTCATGAGAGGGAA'
-for i in range(0,len(dna),3):
-	print(dna[i:i+3])
+for i in range(0,len(dna),3): # i'm saying for i in the range of reading out the sequence of dna by skipping 3
+	print(dna[i:i+3]) # this should add 3 and skip 3  
 	
 '''
 
