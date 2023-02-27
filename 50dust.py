9a7f792e672edbb843336a5c32e62c6712b8824b
FastV1per <SLeano863@gmail.com>
2023-02-27 14:27:34 -0800

    new

--- 50dust.py
+++ 50dust.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 # 50dust.py
 
 # Write a better version of your 42dust.py program
@@ -20,6 +18,7 @@
 
 # import packages  
 import argparse
+import sys
 import mcb185
 import math
 import random
@@ -31,10 +30,10 @@ parser = argparse.ArgumentParser(description ='Entropy Calculator')
 # The positional argument that is always required 
 parser.add_argument('file', type=str, metavar='<path>', help='fasta file')
 
-# Adding to additonal arugments
+# Adding to additional arugments
 parser.add_argument('-w', required=False, type=int, default=11, metavar='<int>', help='requires a integer value [%(default)i]')
-parser.add_argument('-t', required=False, type=float, default = 1.4, metavar='<float>', help= 'requires a float value [%(default).3f]')
-parser.add_argument('-s', action = 'store_true', help = 'lower-based masking')
+parser.add_argument('-t', required=False, type=float, default = 1.4, metavar='<float>', help= 'requires a float value [%(default).2f]')
+parser.add_argument('-s', required=False, action = 'store_true', help = 'lower-based masking [off]')
 
 # Make sure to finalize the arguments
 arg = parser.parse_args()
@@ -46,64 +45,67 @@ arg = parser.parse_args()
 def entroequ(window):
 	
 	# Create containers for the equation 
-	nts = 'ATCG' # Identifying the nucleotides
+	nts = ['A','T','C','G'] # Identifying the nucleotides
 	counts = [0]*4 # how many are in a DNA sequence
 	prob = [] 
+	H = 0
 	
-	for remain in window:
-		for nt in nts:
-			if remain == nt: counts[nts.index(nt)] += 1
+	for nt in window:
+		if nt == nts: 
+			counts[nts.index(nt)] += 1
 			
 	for count in counts:
 		prob.append(count/len(window))
 		
 			
 	# same equation as the previous python code
-	H =  0
 	for probnt in prob:
 		if probnt != 0:
-			H += -(probnt * math.log2(probnt))
+			H -= probnt * math.log2(probnt)
 			
 			
 	return H
 
 
-def sliding(sequ, winlen, threshold):
+def sliding(sequ, winlen):
 
-	file_seq = []
+	win = sequ[:winlen].upper()
+
+	file_seq = sequ
 	
-	for pos in range(len(sequ)-winlen +1):
 	
-		window = sequ[pos:pos + winlen]
-		H = scal(window)
-		
-		if H >= threshold: 
-			file_seq.append(sequ[pos])
-		else:
-			file_seq.append('N')
-		
-		if len(file_seq) >= 60:
-			yield(''.join(file_seq))
-			file_seq = []
+	for pos in range(len(seq)):
 	
-	for nt in range(len(sequ)-winlen + 1, len(sequ)):
-		file_seq.append(sequ[nt])
+		if pos in range(len(sequ)-winlen +1):
+			
+			if pos != 0:
+				win = (win[1:]+sequ[pos+winlen-1]).upper()
+				
+			H = entroequ(win)
+			
 		
-		if len(file_seq) >= 60:
-			yeild(''.join(file_seq))
-			file_seq = []
+			if H < arg.t and arg.s == True:
+				file_seq = file_seq.replace(file_seq[pos:pos+winlen], file_seq[pos:pos + winlen].lower()) 
+
+			elif H < arg.t and arg.s == False:
+				file_seq = file_seq.replace(file_seq[pos:pos+winlen], 'N'*winlen)
+				
+		if (pos + 1) % 60 == 0:
+			yield(file_seq[pos-59:pos+1])
+		
+		if len(sequ) % 60 != 0:
+			yield(file_seq[len(sequ)-(len(sequ) % 60):])
+	
 
 
 # Separating files 
-sequ = sys.argv[1]
-winlen = int(sys.argv[2])
-threshold = float(sys.argv[3])
 
-for seq in mcb185.read.fasta(sequ):
-	print('>' + sequ[0])
-	for line in sequ[1:]:
-		for nts in sliding(line, winlen, threshold):
-			print(nts)
+for line in mcb185.read_fasta(arg.file):
+	print('>' + line[0])
+	
+	for seq in line[1:]:
+		for line in sliding(seq,arg.w):
+			print(line)
 	
 """
 python3 50dust.py -w 11 -t 1.4 -s e.coli.fna  | head
