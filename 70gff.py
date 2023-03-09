96a18f35a31919990ba33601434e1eaf9088944b
FastV1per <SLeano863@gmail.com>
2023-03-08 16:37:15 -0800

    new

---
+++ 70gff.py
@@ -0,0 +1,31 @@
+# 70gff.py
+
+# Write a program that converts genes in gff into JSON
+# Use the E. coli genome gff
+# Your code should mimic the output below
+
+import json
+
+with open('GCF_000005845.2_ASM584v2_genomic.gff.gz') as fp:
+	files = json.load(fp)
+	for data in files:
+		print(data["gene"])
+
+
+"""
+python3 70gff.py ~/DATA/E.coli/GCF_000005845.2_ASM584v2_genomic.gff.gz
+[
+    {
+        "gene": "thrL",
+        "beg": 190,
+        "end": 255,
+        "strand": "+"
+    },
+    {
+        "gene": "thrA",
+        "beg": 337,
+        "end": 2799,
+        "strand": "+"
+    },
+...
+"""
