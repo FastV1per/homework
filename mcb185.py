6ba719fbc77cfe4942c767dfb4b1172069191532
FastV1per <SLeano863@gmail.com>
2023-03-14 00:07:21 -0700

    new

--- mcb185.py
+++ mcb185.py
@@ -29,4 +29,37 @@ def read_fasta(filename):
 	yield(name, ''.join(seqs))
 	fp.close()
 
-# def other functions...
+def translate(filename):
+ 
+    # Define the genetic code
+	gcode = {
+		'AAA' : 'K',	'AAC' : 'N',	'AAG' : 'K',	'AAT' : 'N',
+		'ACA' : 'T',	'ACC' : 'T',	'ACG' : 'T',	'ACT' : 'T',
+		'AGA' : 'R',	'AGC' : 'S',	'AGG' : 'R',	'AGT' : 'S',
+		'ATA' : 'I',	'ATC' : 'I',	'ATG' : 'M',	'ATT' : 'I',
+		'CAA' : 'Q',	'CAC' : 'H',	'CAG' : 'Q',	'CAT' : 'H',
+		'CCA' : 'P',	'CCC' : 'P',	'CCG' : 'P',	'CCT' : 'P',
+		'CGA' : 'R',	'CGC' : 'R',	'CGG' : 'R',	'CGT' : 'R',
+		'CTA' : 'L',	'CTC' : 'L',	'CTG' : 'L',	'CTT' : 'L',
+		'GAA' : 'E',	'GAC' : 'D',	'GAG' : 'E',	'GAT' : 'D',
+		'GCA' : 'A',	'GCC' : 'A',	'GCG' : 'A',	'GCT' : 'A',
+		'GGA' : 'G',	'GGC' : 'G',	'GGG' : 'G',	'GGT' : 'G',
+		'GTA' : 'V',	'GTC' : 'V',	'GTG' : 'V',	'GTT' : 'V',
+		'TAA' : '*',	'TAC' : 'Y',	'TAG' : '*',	'TAT' : 'Y',
+		'TCA' : 'S',	'TCC' : 'S',	'TCG' : 'S',	'TCT' : 'S',
+		'TGA' : '*',	'TGC' : 'C',	'TGG' : 'W',	'TGT' : 'C',
+		'TTA' : 'L',	'TTC' : 'F',	'TTG' : 'L',	'TTT' : 'F',
+	}
+	
+	protein = ''
+	
+
+	for i in range(0,len(filename), 3):
+		codon = filename[i:i+3]
+		if codon in gcode:
+			aa = gcode[codon]
+			protein += aa
+		else:
+			protein += 'X'
+	return protein
+	
