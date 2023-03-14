b3c972e8ec04ea23a657f9c8147b87d9ac2fe281
FastV1per <SLeano863@gmail.com>
2023-03-14 00:56:04 -0700

    new

--- 70gff.py
+++ 70gff.py
@@ -6,10 +6,36 @@
 
 import json
 
-with open('GCF_000005845.2_ASM584v2_genomic.gff.gz') as fp:
-	files = json.load(fp)
-	for line in files:
-		print(json.dumps(tabs, indent=4))
+def converter(filename):
+	
+	with open('GCF_000005845.2_ASM584v2_genomic.gff.gz' , 'r') as f:
+		lines = f.readlines()
+	
+	genes = []
+	for line in lines:
+		if not line.startswith('#'):
+			fields = line.strip().split('\t')
+			if fields[2] == 'genes':
+				genes = {
+					'gene': fields[0],
+					'beg': fields[1],
+					'end': fields[2],
+					'strand': fields[3]
+					}
+				
+				at_fields = fields[8].split(';')
+				for at_field in at_fields:
+					at_parts = at_field.split('=')
+					at_name = at_parts[0]
+					at_value = at_parts[1]
+					gene['convert'][at_name] = at_value
+				
+				genes.append(gene)
+
+
+json_string = json.dumps(genes, indent = 4)
+print(json_string)
+
 
 """
 python3 70gff.py ~/DATA/E.coli/GCF_000005845.2_ASM584v2_genomic.gff.gz
