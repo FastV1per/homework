6d50d1e141858096a18018f24c680a3912b9dc5d
FastV1per <SLeano863@gmail.com>
2023-03-14 00:17:56 -0700

    new

--- 62orfs.py
+++ 62orfs.py
@@ -15,6 +15,88 @@
 
 # Note: your genes should be similar to those in the real genome
 
+import argparse
+import mcb185 
+import re 
+
+
+
+# Initialize argument parser
+parser = argparse.ArgumentParser(description='Find ORFS in genome that has optional ORF size')
+	
+# Take arguments: fasta file and min orf len
+parser.add_argument('file', type=str, metavar='<path>', help='FASTA file')
+parser.add_argument('-l', type=int, metavar='<int>', required=False,
+	default=300, help='minimum ORF size [%(default)i]')
+
+# Finish argument parser setup
+arg = parser.parse_args()
+
+
+# Finda all possible ORFS
+def get_orfs(iden, strand, seq):
+
+	# Initialize variables
+	anti = {'A':'T', 'T':'A', 'G':'C', 'C':'G'} 
+	
+	# Make anti-sense strand
+	if strand == '-':
+		aseq = ''
+		for nt in seq[::-1]: aseq += anti[nt]
+		seq = aseq
+
+	# Returing codons
+	for i in range(len(seq)-3):
+	
+		# Making sure that the starting frame match the min ORFs
+		if i >= len(seq)-arg.l: 
+			break
+		
+		# Slide window
+		cd = seq[i:i+3]
+		
+		# Get protein seq and translate
+		if cd == 'ATG': 
+		
+			# use mcb185 translate
+			pseq = mcb185.translate(seq[i:])
+			
+			
+			if pseq == None:
+				continue
+			
+		
+			pseq = pseq.split('*')[0]
+			start = i+len(pseq)*3+3 # start of the orf
+			
+			# Check for min len
+			if len(pseq) < arg.l/3 or pseq == None:
+				continue
+			
+			# Report ORF to its length
+			elif strand == '+':
+				yield(iden, i+1, start, '+', pseq[:10])
+				continue
+			elif strand == '-':
+				yield(iden, len(seq)-start+1, len(seq)-i, '-', pseq[:10])
+				continue
+
+
+# Read fasta file and then get given results
+for desc, seq in mcb185.read_fasta(arg.file):
+
+	# Obtain the identifier for the sequence
+	iden = re.search('\w+.\S+', desc)
+	iden = iden.group()
+
+	# Report the orf stats for each strand
+	for iden, start, end, strand, pseq in get_orfs(iden, '+', seq):
+		print(iden, start, end, strand, pseq)
+	for iden, start, end, strand, pseq in get_orfs(iden, '-', seq):
+		print(iden, start, end, strand, pseq)
+
+
+
 
 """
 python3 62orfs.py ~/DATA/E.coli/GCF_000005845.2_ASM584v2_genomic.fna.gz
