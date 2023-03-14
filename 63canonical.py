8e9a0fd6e4c46fd8541acb62368916b84f92cec0
FastV1per <SLeano863@gmail.com>
2023-03-13 23:29:59 -0700

    new

---
+++ 63canonical.py
@@ -0,0 +1,48 @@
+# 63canonical.py
+
+# You probably learned that ATG is the only start codon, but is it?
+# Write a program that reports the start codons from the E. coli genome
+# Your program must:
+#    1. read GCF_000005845.2_ASM584v2_genomic.gbff.gz at the only input
+#    2. use a regex to find CDS coordinates
+#    3. count how many different start codons there are
+
+# Note: the sequence is at the end of the file
+# Note: genes on the negative strand are marked complement(a..b)
+
+# Hint: you can read a file twice, first to get the DNA, then the CDS
+# Hint: check your CDS by examining the source protein
+
+import sys
+import gzip
+import re
+import mcb185
+
+fp = gzip.open(sys.argv[1], 'rt')
+
+all = ''
+for line in fp:
+	all += line
+
+pat1 = 'ORIGIN'
+seq = ''
+
+for match in re.finditer(part1, all):
+	seqstart = match.start()-1
+	
+
+"""
+python3 63canonical.py ~/DATA/E.coli/GCF_000005845.2_ASM584v2_genomic.gbff.gz
+ATG 3883
+GTG 338
+TTG 80
+ATT 5
+AGT 1
+AAA 1
+AGC 1
+TTC 1
+TAA 1
+CGT 1
+CTG 2
+GAC 1
+"""
